9be3a1c2c2c9fa70866f766870c3d4da32500082
kanishma99 <kanishma.kani99@gmail.com>
2025-04-28 17:47:10 +0530

    SECOND TIME PUSH

--- src/test/java/com/samsung/Mobile.java
+++ src/test/java/com/samsung/Mobile.java
@@ -1,5 +1,17 @@
 package com.samsung;
 
 public class Mobile {
-
+	private void method() {
+		System.out.println("1");
+		 
+	}
+ private void method2() {
+	 System.out.println("2");
+	 
 }
+ 
+	 private void method3() {
+		 System.out.println("3");
+		 
+	}
+ }
